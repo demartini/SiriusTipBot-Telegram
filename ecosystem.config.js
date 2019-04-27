49ed8a812b78e5263c8a2a0a219d11f2549655c3
Iolar Demartini Junior <iolardemartini@gmail.com>
2019-04-27 16:23:44 -0300

    Initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "SiriusTipBot-Telegram",
+      script: "./SiriusTip.py",
+      interpreter: "/usr/bin/python",
+      output: '/home/siriusteam/tipbot-telegram/logs/out.log',
+      error: '/home/siriusteam/tipbot-telegram/logs/error.log',
+      log: '/home/siriusteam/tipbot-telegram/logs/combined.outerr.log',
+      watch: true,
+      max_memory_restart: '1G'
+    }
+  ]
+}
